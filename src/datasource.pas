10014630b4608d67ca643df1cbc82607187cadd9
Fajar Julyana <fajarjulyana1@gmail.com>
2023-10-08 21:35:32 +0700

    backup db.mdb InvoiceGenerator InvoiceGenerator.ico InvoiceGenerator.lpi InvoiceGenerator.lpr InvoiceGenerator.lps InvoiceGenerator.res lib LICENSE README.md src added

--- src/datasource.pas
+++ src/datasource.pas
@@ -5,14 +5,17 @@ unit DataSource;
 interface
 
 uses
-  Classes, SysUtils, DB;
+  Classes, SysUtils, DB, SQLDB, odbcconn, oracleconnection;
 
 type
 
   { TDataModule1 }
 
   TDataModule1 = class(TDataModule)
+    SQLConnector1: TSQLConnector;
+    SQLQuery1: TSQLQuery;
     SumberData: TDataSource;
+    procedure SQLConnector1AfterConnect(Sender: TObject);
   private
 
   public
@@ -25,6 +28,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TDataModule1 }
+
+procedure TDataModule1.SQLConnector1AfterConnect(Sender: TObject);
+begin
+
+end;
 
 end.
 
